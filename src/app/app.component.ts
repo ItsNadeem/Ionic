c5bfed6bf63362347a566216394f11cd332c7436
Nadeem Abdul <nadeemabdul@Nadeems-MacBook-Pro.local>
2018-03-06 18:41:37 -0500

    login page is the landing page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Platform, Nav } from 'ionic-angular';
+import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
@@ -39,7 +39,6 @@ export class MyApp {
   }
 
   platformReady() {
-
     // Calls any initial plugins when ready
     // Here you can do any higher level native things you might need.
     this.platform.ready().then(() => {
--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,7 +1,6 @@
-import { Component } from '@angular/core';
-import { NavController, NavParams, MenuController} from 'ionic-angular';
+import { Component, ViewChild } from '@angular/core';
+import { AlertController, NavController, MenuController, LoadingController, Slides } from 'ionic-angular';
 import { HomePage } from '../home/home';
-
 
 
 @Component({
@@ -9,21 +8,65 @@ import { HomePage } from '../home/home';
   templateUrl: 'login.html',
 })
 export class LoginPage {
+  public backgroundImage = 'assets/img/background/background-6.jpg';
 
   constructor(public navCtrl: NavController,
-              public menu: MenuController,) { }
+              public menu: MenuController,
+              public loadingCtrl: LoadingController,
+              public alertCtrl: AlertController) { }
+  // Slider methods
+  @ViewChild('slider') slider: Slides;
+  @ViewChild('innerSlider') innerSlider: Slides;
 
-  startApp() {
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad LoginPage');
+    this.menu.enable(false);
+  }
+
+  goToLogin() {
+    this.slider.slideTo(1);
+  }
+
+  goToSignup() {
+    this.slider.slideTo(2);
+  }
+
+  slideNext() {
+    this.innerSlider.slideNext();
+  }
+
+  slidePrevious() {
+    this.innerSlider.slidePrev();
+  }
+
+  presentLoading(message) {
+    const loading = this.loadingCtrl.create({
+      duration: 100
+    });
+
+    loading.onDidDismiss(() => {
+      const alert = this.alertCtrl.create({
+        title: 'Success',
+        subTitle: message,
+        buttons: ['Dismiss']
+      });
+      alert.present();
+    });
+
+    loading.present();
+  }
+
+  login() {
+    this.presentLoading('Thanks for signing up!');
     this.navCtrl.push(HomePage);
     this.menu.enable(true);
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad LoginPage');
+  signup() {
+    this.presentLoading('Thanks for signing up!');
+    // this.navCtrl.push(HomePage);
   }
-
-  ionViewDidEnter() {
-    // the left menu on root should be disabled on the login page
-    this.menu.enable(false);
+  resetPassword() {
+    this.presentLoading('An e-mail was sent with your new password.');
   }
 }
